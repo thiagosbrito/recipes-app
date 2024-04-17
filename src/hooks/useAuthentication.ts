97a9219bbd4f3ee98f787fad9269ea070739ff13
Thiago Brito <thiago83brito@hotmail.com>
2024-04-18 00:37:26 +0200

    fixing window is not defined

--- src/hooks/useAuthentication.ts
+++ src/hooks/useAuthentication.ts
@@ -1,6 +1,7 @@
 import * as RoutesConstants from '@/constants/routes';
 import { AuthContext } from '@/providers/AuthProvider';
 import { useRouter } from 'next/navigation';
+import { useRouter as useRouterNext } from 'next/router';
 import { useEffect } from 'react';
 
 const GUEST_ROUTES = [
@@ -15,7 +16,8 @@ const useAuthentication = () => {
     const { user }: any = AuthContext();
     const userInfo = user?.user || null;
     const router = useRouter();
-    const currentRoute = window.location.pathname;
+    const route = useRouterNext();
+    const currentRoute = route.pathname;
 
     useEffect(() => {
         if (!userInfo) {
